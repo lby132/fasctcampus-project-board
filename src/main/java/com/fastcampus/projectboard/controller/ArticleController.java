a4d9b7d5de83be1960f415ed98bcc9d7301d20c4
lby132 <quddbscjswow@naver.com>
2024-05-20 23:54:57 +0900

    게시글 관련 뷰 테스트 작성 우선 어떤 경로로 뷰를 열어줄지 테스트로 명세서를 뽑는다. 구현중인 테스트가 실패하면서 기능 구현중인 유닛테스트는 @Disabled 처리함

---
+++ src/main/java/com/fastcampus/projectboard/controller/ArticleController.java
@@ -0,0 +1,16 @@
+package com.fastcampus.projectboard.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * /articles
+ * /articles/{articles-id}
+ * /articles/search
+ * /articles/search-hashtag
+ */
+@RequestMapping("/article")
+@Controller
+public class ArticleController {
+
+}
