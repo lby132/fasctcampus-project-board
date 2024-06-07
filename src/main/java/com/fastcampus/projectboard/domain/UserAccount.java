85cfb98e7f617587b790e3c320bdbd154065a8d5
lby132 <quddbscjswow@naver.com>
2024-06-08 00:04:37 +0900

    페이징 처리 화면

--- src/main/java/com/fastcampus/projectboard/domain/UserAccount.java
+++ src/main/java/com/fastcampus/projectboard/domain/UserAccount.java
@@ -10,6 +10,7 @@ import java.util.Objects;
 @Getter
 @ToString
 @Table(indexes = {
+        @Index(columnList = "userId"),
         @Index(columnList = "email", unique = true),
         @Index(columnList = "createdAt"),
         @Index(columnList = "createdBy")
