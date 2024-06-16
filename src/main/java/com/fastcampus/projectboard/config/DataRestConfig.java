2eae1e22153cb2078599f8839bfdf0d245e145f9
lby132 <quddbscjswow@naver.com>
2024-06-17 00:13:30 +0900

    DataRestConfig 생성

--- src/main/java/com/fastcampus/projectboard/config/DataRestConfig.java
+++ src/main/java/com/fastcampus/projectboard/config/DataRestConfig.java
@@ -1,2 +1,18 @@
-package com.fastcampus.projectboard.config;public class DataRestConfig {
+package com.fastcampus.projectboard.config;
+
+import com.fastcampus.projectboard.domain.UserAccount;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+
+@Configuration
+public class DataRestConfig {
+
+    @Bean
+    public RepositoryRestConfigurer repositoryRestConfigurer() {
+        return RepositoryRestConfigurer.withConfig((config, cors) ->
+                config.exposeIdsFor(UserAccount.class)
+        );
+    }
+
 }
