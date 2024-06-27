39c4a98807a393d3f010e8ea871debb65c257454
lby132 <quddbscjswow@naver.com>
2024-06-27 23:55:30 +0900

    PathRequest.toStaticResources().atCommonLocations()을 ignoring()을 하기 때문에 csrf를 하지 않기 때문에 취약할 수 있어서 permitAll을 시키기 위해 securityFilterChain 메서드 안으로 옮김

--- src/main/java/com/fastcampus/projectboard/config/SecurityConfig.java
+++ src/main/java/com/fastcampus/projectboard/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import com.fastcampus.projectboard.dto.UserAccountDto;
 import com.fastcampus.projectboard.dto.security.BoardPrincipal;
 import com.fastcampus.projectboard.repository.UserAccountRepository;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.boot.autoconfigure.security.servlet.StaticResourceRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -28,7 +29,8 @@ public class SecurityConfig {
                                 HttpMethod.GET,
                                 "/",
                                 "/articles",
-                                "/articles/search-hashtag"
+                                "/articles/search-hashtag",
+                                PathRequest.toStaticResources().atCommonLocations().toString()
                         ).permitAll()
                         .anyRequest().permitAll()
                 )
@@ -37,13 +39,14 @@ public class SecurityConfig {
                 ).build();
     }
 
-    @Bean
-    public WebSecurityCustomizer webSecurityCustomizer() {
-        // 스프링 시큐리티 검사에서 제외하겠다는 뜻
-        // 예를 들면 static, resource, css - js 등 검사할 필요가 없는것들 추가
-        // PathRequest.toStaticResources().atCommonLocations() static 리소스의 모든 로케이션을 잡는다.
-        return (web) -> web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
-    }
+//    @Bean
+//    public WebSecurityCustomizer webSecurityCustomizer() {
+//        // 스프링 시큐리티 검사에서 제외하겠다는 뜻
+//        // 예를 들면 static, resource, css - js 등 검사할 필요가 없는것들 추가
+//        // PathRequest.toStaticResources().atCommonLocations() static 리소스의 모든 로케이션을 잡는다.
+            // 여기서는 ignoring()을 하기 때문에 csrf를 하지 않기 때문에 취약할 수 있어서 permitAll을 시키기 위해 securityFilterChain 으로 옮기는게 더 좋은코드라고 함.
+//        return (web) -> web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+//    }
 
     @Bean
     public UserDetailsService userDetailsService(UserAccountRepository userAccountRepository) {
