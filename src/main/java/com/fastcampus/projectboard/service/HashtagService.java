4b41fdab43fa99701154d4db3a7892a88275dbb5
lby132 <quddbscjswow@naver.com>
2024-07-18 23:38:54 +0900

    hashtag service 테스트코드 작성

--- src/main/java/com/fastcampus/projectboard/service/HashtagService.java
+++ src/main/java/com/fastcampus/projectboard/service/HashtagService.java
@@ -1,16 +1,30 @@
 package com.fastcampus.projectboard.service;
 
+import com.fastcampus.projectboard.domain.Hashtag;
+import com.fastcampus.projectboard.repository.HashtagRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
+import java.util.regex.Pattern;
 
 @Service
+@RequiredArgsConstructor
 public class HashtagService {
-    public Object parseHashtagNames(String content) {
+
+    private final HashtagRepository hashtagRepository;
+
+    public Set<String> parseHashtagNames(String content) {
+        if (content == null) {
+            return Set.of();
+        }
+
+//        Pattern.compile()
+
         return null;
     }
 
-    public Object findHashtagsByNames(Set<String> expectedHashtagNames) {
+    public Set<Hashtag> findHashtagsByNames(Set<String> expectedHashtagNames) {
         return null;
     }
 
