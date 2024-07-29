1d15577174a86e36a5eff2afcf4eb1d0887e4d07
lby132 <quddbscjswow@naver.com>
2024-07-29 23:41:57 +0900

    카카오 인증 서비스 코드 구현

--- src/main/java/com/fastcampus/projectboard/config/SecurityConfig.java
+++ src/main/java/com/fastcampus/projectboard/config/SecurityConfig.java
@@ -13,14 +13,22 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(
+            HttpSecurity http,
+            OAuth2UserService<OAuth2UserRequest, OAuth2User> oAuth2UserService
+    ) throws Exception {
         return http
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
@@ -32,14 +40,18 @@ public class SecurityConfig {
                         ).permitAll()
                         .anyRequest().authenticated()
                 )
-                .formLogin(formLogin -> formLogin
-                        .loginPage("/login")
-                        .permitAll()
+//                .formLogin(formLogin -> formLogin
+//                        .loginPage("/login")
+//                        .permitAll()
+//                )
+                .formLogin(withDefaults()) // withDefaults()로 해두면 아무것도 하지않는다.
+                .logout((logout) -> logout.logoutSuccessUrl("/"))
+                .oauth2Login(oAuth -> oAuth
+                        .userInfoEndpoint(userInfo -> userInfo
+                                .userService(oAuth2UserService)
+                        )
                 )
-                .logout((logout) -> logout
-                        .logoutSuccessUrl("/")
-                        .permitAll()
-                ).build();
+                .build();
     }
 
 //    @Bean
@@ -58,6 +70,11 @@ public class SecurityConfig {
                 .map(UserAccountDto::from)
                 .map(BoardPrincipal::from)
                 .orElseThrow(() -> new UsernameNotFoundException("유저를 찾을 수 없습니다. - username: " + username));
+    }
+
+    @Bean
+    public OAuth2UserService<OAuth2UserRequest, OAuth2User> oAuth2UserService() {
+
     }
 
     @Bean
--- src/test/java/com/fastcampus/projectboard/dto/security/KakaoOAuthResponseTest.java
+++ src/test/java/com/fastcampus/projectboard/dto/security/KakaoOAuthResponseTest.java
@@ -41,8 +41,7 @@ class KakaoOAuthResponseTest {
                     }
                 }
                 """;
-        Map<String, Object> attributes = mapper.readValue(serializedResponse, new TypeReference<>() {
-        });
+        Map<String, Object> attributes = mapper.readValue(serializedResponse, new TypeReference<>() {});
 
         //when
         KakaoOAuth2Response result = KakaoOAuth2Response.from(attributes);
