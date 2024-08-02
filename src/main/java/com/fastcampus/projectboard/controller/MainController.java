c2d329836120b236a4e410c5d069a9f66805bb06
lby132 <quddbscjswow@naver.com>
2024-08-02 23:33:36 +0900

    swagger 설정

--- src/main/java/com/fastcampus/projectboard/controller/MainController.java
+++ src/main/java/com/fastcampus/projectboard/controller/MainController.java
@@ -1,7 +1,12 @@
 package com.fastcampus.projectboard.controller;
 
+import com.fastcampus.projectboard.dto.response.ArticleCommentResponse;
+import com.fastcampus.projectboard.dto.response.ArticleResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.time.LocalDateTime;
 
 @Controller
 public class MainController {
@@ -9,5 +14,18 @@ public class MainController {
     @GetMapping("/")
     public String root() {
         return "redirect:/articles";
+    }
+
+    @ResponseBody
+    @GetMapping("/test-rest")
+    public ArticleCommentResponse test(Long id) {
+        return ArticleCommentResponse.of(
+                id,
+                "content",
+                LocalDateTime.now(),
+                "e@mail.com",
+                "Uno",
+                "uno"
+        );
     }
 }
